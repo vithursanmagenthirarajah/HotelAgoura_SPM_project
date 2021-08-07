12f4255e4220793adc43bf665e58e0aca5fa70d6
Vithursan Magenthirarajah <57103371+vithursanmagenthirarajah@users.noreply.github.com>
2021-08-07 16:01:37 +0530

    SPM project creation completed

---
+++ src/test/java/com/Agoura/HotelAgoura/HotelAgouraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Agoura.HotelAgoura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAgouraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
